71a42d5c451cd65d4d46bc94f7942025874be431
Dnyanesh-Gavali <u25ai054@aid.svnit.ac.in>
2026-01-31 18:13:12 +0530

    feat: DeepFake Detection using XCEPTIONNET

--- forensics/deepfake.py
+++ forensics/deepfake.py
@@ -1,30 +1,58 @@
 from typing import Dict, Any
 from .base import ForensicTool
-import random
+
+import tensorflow as tf
+from huggingface_hub import hf_hub_download
+import numpy as np
+from PIL import Image
 
 
 class DeepfakeDetector(ForensicTool):
-    def analyze(self, media_path: str, claim_text: str = "") -> Dict[str, Any]:
-        # TODO: Integrate with an actual deepfake detection model.
 
-        # For the "Shark" example, we want a low score (Real Photo)
-        # TODO: Simulated logic for demonstration purposes
-        if "shark" in media_path.lower() or "shark" in claim_text.lower():
+    def __init__(self):
+        model_path = hf_hub_download(
+            repo_id="Dnyanesh-Gavali07/XCEPTIONNET-DeepFake-Detector",
+            filename="xception_deepfake_model.keras"
+        )
+
+        self.model = tf.keras.models.load_model(model_path)
+
+        # Image input size for Xception
+        self.img_size = (299, 299)
+
+
+    def preprocess(self, image_path: str):
+        img = Image.open(image_path).convert("RGB")
+        img = img.resize(self.img_size)
+
+        arr = np.array(img) / 255.0
+        arr = np.expand_dims(arr, axis=0)
+
+        return arr
+
+    def analyze(self, media_path: str, claim_text: str = "") -> Dict[str, Any]:
+        try:
+            img = self.preprocess(media_path)
+
+            # Model prediction
+            pred = self.model.predict(img, verbose=0)[0][0]
+
+            fake_prob = float(pred)
+
             return {
                 "tool": "Deepfake_Detector",
-                "fake_prob": 0.1,
-                "verdict": "Likely Real Camera Image",
+                "fake_prob": round(fake_prob, 4),
+                "verdict": (
+                    "Likely AI-Generated"
+                    if fake_prob > 0.5
+                    else "Likely Real Camera Image"
+                ),
             }
 
-        # Default random simulation
-        prob = random.uniform(0, 0.2)
-        return {
-            "tool": "Deepfake_Detector",
-            "fake_prob": round(prob, 2),
-            "verdict": (
-                "NOTE: THIS IS A SIMULATED RESULT. DO NOT USE FOR REAL ANALYSIS. "
-                "Likely Real Camera Image"
-                if prob < 0.5
-                else "Likely AI-Generated"
-            ),
-        }
+        except Exception as e:
+            return {
+                "tool": "Deepfake_Detector",
+                "error": str(e),
+                "fake_prob": None,
+                "verdict": "Analysis Failed",
+            }
